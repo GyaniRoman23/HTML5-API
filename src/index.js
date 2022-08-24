cc8d88a714990bf22f9bf9c70b455fdf4f050462
GyaniRoman23 <gyaneshsingh2003111@gmail.com>
2022-08-24 14:08:15 +0530

    learning draggable API and its various methods and how to implement them

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
+
+const init = () => { };
+
+console.dir(document.createElement('div'));
+if ('dragglble' in document.createElement('div')) {
+    init();
+}
